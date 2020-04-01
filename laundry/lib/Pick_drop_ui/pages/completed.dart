4e78fc33934227540c5c0e28fa3618d9087e1a35
sanjog12 <57636113+sanjog12@users.noreply.github.com>
2020-04-01 22:01:03 +0530

    Bottom navigation Bar and page_control

---
+++ laundry/lib/Pick_drop_ui/pages/completed.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Completed extends StatefulWidget{
+	@override
+	State<StatefulWidget> createState() {
+		// TODO: implement createState
+		return _completed();
+	}
+}
+
+class _completed extends State<Completed>{
+	@override
+	Widget build(BuildContext context) {
+		// TODO: implement build
+		return Container(
+			child: Text("History Section"),
+		);
+	}
+}
